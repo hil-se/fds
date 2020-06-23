e8c58b3c5ce508e415b0b1f8cca826f1f68aca3a
Zhe Yu <zyu9@ncsu.edu>
2020-06-22 21:33:32 -0400

    assignment 4

---
+++ assignments/assignment4/A4.py
@@ -0,0 +1,23 @@
+from my_KNN import my_KNN
+import pandas as pd
+
+if __name__ == "__main__":
+    #  Load training data
+    data_train = pd.read_csv("../data/Iris_train.csv")
+    # Separate independent variables and dependent variables
+    independent = ["SepalLengthCm",	"SepalWidthCm",	"PetalLengthCm",	"PetalWidthCm"]
+    X = data_train[independent]
+    y = data_train["Species"]
+    # Train model
+    clf = my_KNN()
+    clf.fit(X,y)
+    # Load testing data
+    data_test = pd.read_csv("../data/Iris_test.csv")
+    X_test = data_test[independent]
+    # Predict
+    predictions = clf.predict(X_test)
+    # Predict probabilities
+    probs = clf.predict_proba(X_test)
+    # Print results
+    for i,pred in enumerate(predictions):
+        print("%s\t%f" %(pred,max(probs[i])))
---
+++ assignments/assignment4/my_KNN.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+class my_KNN:
+
+    def __init__(self, n_neighbors=5, metric="minkowski", p=2):
+        # metric = {"minkowski", "euclidean", "manhattan", "cosine"}
+        # p value only matters when metric = "minkowski"
+        # notice that for "cosine", 1 is closest and -1 is furthest
+        # therefore usually cosine_dist = 1- cosine(x,y)
+        self.n_neighbors = n_neighbors
+        self.metric = metric
+        self.p = p
+
+    def fit(self, X, y):
+        # X: pd.DataFrame, independent variables, float
+        # y: list, np.array or pd.Series, dependent variables, int or str
+        self.classes_ = list(set(list(y)))
+        # write your code below
+        return
+
+    def predict(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # return predictions: list
+        # write your code below
+        return predictions
+
+    def predict_proba(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # return probs = list of list
+        # write your code below
+        return probs
+
+
+
