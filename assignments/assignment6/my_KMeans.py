9818eade0731ff5a0d446214a2051d1cc10a9612
Zhe Yu <zyu9@ncsu.edu>
2020-08-21 11:09:34 -0400

    Update my_KMeans.py

--- assignments/assignment6/my_KMeans.py
+++ assignments/assignment6/my_KMeans.py
@@ -3,12 +3,15 @@ import numpy as np
 
 class my_KMeans:
 
-    def __init__(self, n_clusters=8, init = "k-means++", max_iter=300, tol=1e-4):
+    def __init__(self, n_clusters=8, init = "k-means++", n_init = 10, max_iter=300, tol=1e-4):
         # init = {"k-means++", "random"}
         # stop when either # iteration is greater than max_iter or the delta
         # of self.inertia_ is smaller than tol.
+        # repeat n_init times and keep the best run (cluster_centers_, inertia_) with the lowest inertia_.
+        # Hint: memorize the calculated distances.
         self.n_clusters = int(n_clusters)
         self.init = init
+        self.n_init = n_init
         self.max_iter = int(max_iter)
         self.tol = tol
 
@@ -19,7 +22,9 @@ class my_KMeans:
         self.inertia_ = None
 
     def fit(self, X):
-        # X: pd.DataFrame, independent variables, float
+        # X: pd.DataFrame, independent variables, float        
+        # repeat self.n_init times and keep the best run 
+            # (self.cluster_centers_, self.inertia_) with the lowest self.inertia_.
         # write your code below
         return
 
