195bf5b4b60ac1b54fe4bcc31024f72cebb28ff3
Zhe Yu <zyu9@ncsu.edu>
2020-08-24 09:07:48 -0400

    Update my_DT.py

--- assignments/assignment2/my_DT.py
+++ assignments/assignment2/my_DT.py
@@ -2,18 +2,16 @@ import pandas as pd
 
 class my_DT:
 
-    def __init__(self, criterion="gini", max_depth=8, min_impurity_decrease=0, min_samples_split=2, min_samples_leaf=1):
+    def __init__(self, criterion="gini", max_depth=8, min_impurity_decrease=0, min_samples_split=2):
         # criterion = {"gini", "entropy"},
         # Do not split and stop training if
         # (1) depth > max_depth after split (when only the root, depth = 0)
         # (2) the decrease of impurity < min_impurity_decrease
         # Only split node with >= min_samples_split samples
-        # to leaves with >= min_samples_leaf samples
         self.criterion = criterion
         self.max_depth = int(max_depth)
         self.min_impurity_decrease = min_impurity_decrease
         self.min_samples_split = int(min_samples_split)
-        self.min_samples_leaf = int(min_samples_leaf)
 
     def fit(self, X, y):
         # X: pd.DataFrame, independent variables, float
