0dcc6e0d6152395a3791f35c0abc5724dbca7b8b
azhe825 <zyu9@ncsu.edu>
2020-09-13 20:28:18 -0400

    A5

---
+++ assignments/assignment5/my_AdaBoost_hint.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import numpy as np
+from copy import deepcopy
+from pdb import set_trace
+
+class my_AdaBoost:
+
+    def __init__(self, base_estimator = None, n_estimators = 50):
+        # base_estimator: the base classifier class, e.g. my_DT
+        # n_estimators: # of base_estimator rounds
+        self.base_estimator = base_estimator
+        self.n_estimators = int(n_estimators)
+        self.estimators = [deepcopy(self.base_estimator) for i in range(self.n_estimators)]
+
+
+    def fit(self, X, y):
+        # X: pd.DataFrame, independent variables, float
+        # y: list, np.array or pd.Series, dependent variables, int or str
+
+        self.classes_ = list(set(list(y)))
+        n = len(y)
+        w = np.array([1.0/n]*n)
+        labels = np.array(y)
+        self.alpha = []
+        for i in range(self.n_estimators):
+            # Sample with replacement from X, with probability w
+            sample = np.random.choice(n,n,p=w)
+            # Train base classifier with sampled training data
+            sampled = X.iloc[sample]
+            sampled.index = range(len(sample))
+            self.estimators[i].fit(sampled, labels[sample])
+            predictions = self.estimators[i].predict(X)
+            diffs = np.array(predictions) != y
+            # Compute error rate for estimator i
+            error = np.sum(diffs * w)
+            while error > 0.5:
+                w = np.array([1.0 / n] * n)
+                sample = np.random.choice(n, n, p=w)
+                # Train base classifier with sampled training data
+                sampled = X.iloc[sample]
+                sampled.index = range(len(sample))
+                self.estimators[i].fit(sampled, labels[sample])
+                predictions = self.estimators[i].predict(X)
+                diffs = np.array(predictions) != y
+                # Compute error rate and alpha for estimator i
+                error = np.sum(diffs * w)
+            # Compute alpha for estimator i
+            self.alpha.append("write your own code")
+
+            # Update wi
+            w = "write your own code"
+
+        # Normalize alpha
+        self.alpha = self.alpha / np.sum(self.alpha)
+        return
+
+    def predict(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # return predictions: list
+        probs = self.predict_proba(X)
+        predictions = [self.classes_[np.argmax(prob)] for prob in probs.to_numpy()]
+        return predictions
+
+    def predict_proba(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # prob: what percentage of the base estimators predict input as class C
+        # prob(x)[C] = sum(alpha[j] * (base_model[j].predict(x) == C))
+        # return probs = pd.DataFrame(list of prob, columns = self.classes_)
+        # write your code below
+        probs = {}
+        for label in self.classes_:
+            # Calculate probs for each label
+            "write your own code"
+
+
+
+        probs = pd.DataFrame(probs, columns=self.classes_)
+        return probs
+
+
+
+
+
