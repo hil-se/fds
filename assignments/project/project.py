3e529d6d4d31c7cd04b8942b364ac8ae7b5463e9
Zhe Yu <zhe@zhe-yus-mbp.wireless.rit.edu>
2020-10-15 13:43:04 -0400

    project

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -1,24 +1,30 @@
 import pandas as pd
 import time
 
-def train_model(data):
-    # Train a model to predict fake news
-    y = data["fraudulent"]
-    start_time = time.time()
-    while time.time() - start_time < 25 * 60:
-        # Iteration of model training or hyperparameter tuning
-    return clf
+class my_model():
+    def fit(self, X, y):
+        # do not exceed 29 mins
+        return
+
+    def predict(self, X):
+        return predictions
 
 if __name__ == "__main__":
+    start = time.time()
     # Load data
     data = pd.read_csv("../data/job_train.csv")
     # Replace missing values with empty strings
     data = data.fillna("")
+    y = data["fraudulent"]
+    X = data.drop(['fraudulent'], axis=1)
     # Train model
-    clf = train_model(data)
+    clf = my_model()
+    clf.fit(X, y)
+    runtime = (time.time() - start) / 60.0
+    print(runtime)
 
 
 
-    
-    
+
+
 
